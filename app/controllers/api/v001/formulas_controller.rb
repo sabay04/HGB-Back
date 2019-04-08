120eadead7b0dc6a728cdb5b6ac5ccaa315927e3
github username <github email address>
2019-04-08 17:47:58 +0100

    edit and update formula

--- app/controllers/api/v001/formulas_controller.rb
+++ app/controllers/api/v001/formulas_controller.rb
@@ -1,5 +1,5 @@
 class Api::V001::FormulasController < ApplicationController
-
+    before_action :find_ingredient only: [ :update, :destroy]
 
     # full crud 
     def index
@@ -33,12 +33,52 @@ class Api::V001::FormulasController < ApplicationController
         end 
     end 
 
+    def update 
+        # @formula = Formula.find(params[:id])
+        @formula.update(formula_params)
+    
+        if @formula.valid?
+
+            params[:formula][:ingredients].map do |ing| 
+                @ingredient = Ingredient.find_by(name: ing[:name])
+                @formulaIngredient =  FormulaIngredient.find_by(formula_id: @formula.id, ingredient_id: @ingredient.id)
+              
+                if @formulaIngredient 
+                    @formulaIngredient.update(formula_id: @formula.id, ingredient_id: @ingredient.id, percentage: ing[:percentage])
+               
+                else
+                    @formulaIngredient =  FormulaIngredient.create(formula_id: @formula.id, ingredient_id: @ingredient.id, percentage: ing[:percentage])
+                end 
+
+            end 
+            
+            params[:formula][:concerns].map do |con| 
+                @concern = Concern.find_by(name: con[:name])
+                @formulaConcern = FormulaConcern.find_or_create_by(formula_id: @formula.id, concern_id: @concern.id)
+            end 
+          
+
+            render json: @formula
+        else
+            render json: "formula parameters invalid"
+        end
+    end 
+
+
+    def destroy
+        # @formula = Formula.find(params[:id])
+        @formula.destroy
+    end 
 
 
     private 
 
     def formula_params
       params.require(:formula).permit(:title, :description, :image, :user_id, :category_id, :directions)
+    end 
+
+    def find_formula 
+        @formula = Formula.find(params[:id])
     end 
 
 end
